2e32023485875a017fdad62f8d478f12ab9f2a91
Tim Triemstra <root@timtr.com>
2026-03-15 14:15:34 -0700

    Fixed a mistake / typo in the functinos file for Linux and Mac

--- Mac/Root/dot-functions.sh
+++ Mac/Root/dot-functions.sh
@@ -23,7 +23,7 @@ function bullet()  { printf "\r\033[00;36m   ==\033[0m $1 \n" }
 function findall() { find . -name "$1" -print 2> /dev/null; }
 
 # ... grep for a string in the root and recursively downward
-function findstring() { grep -rinl --color "$1" . }
+function findstring() { grep -rinl --color "$1" .; }
 
 # ... Create a new directory and enter it
 function md() { mkdir -p "$@" && cd "$@"; }
