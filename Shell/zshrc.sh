bdcc2018675a8d6d7a4b8b8dbc02a0675500bb48
Tim Triemstra <root@timtr.com>
2026-06-28 11:26:07 -0700

    cleanup the prompt a bit more, and changed some iTerm settings

--- Shell/zshrc.sh
+++ Shell/zshrc.sh
@@ -43,7 +43,8 @@ parse_git_branch() {
 setopt PROMPT_SUBST
 HOSTNAME=$(hostname -s)
 NEWLINE=$'\n'
-PROMPT='${NEWLINE}%F{#ff6a00}% %9c% %f  %F{#203a47}% ${HOSTNAME} %F{#0096ff}% $(parse_git_branch) ${NEWLINE}%F{#203a47}% $USER %F{#eef8ff}% 〉%f'
+PROMPT='${NEWLINE}%F{#ff6a00}% %9c% %f    %F{#203a47}% $USER|${HOSTNAME} %F{#0096ff}% $(parse_git_branch) ${NEWLINE}%F{#eef8ff}% 〉%f'
+
 
 
 # end of file
